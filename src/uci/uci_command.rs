7400aa0fd0640fd6ec2c51db389f1943e39a9363
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-11-29 20:04:03 +0100

    Reimplement simple command parsing

--- src/uci/uci_command.rs
+++ src/uci/uci_command.rs
@@ -2,70 +2,36 @@
 // https://github.com/Heiaha/Weiawaga/blob/493d8139f882b89380c298457267cb059d86dc2f/src/uci/uci.rs
 #[derive(Debug, PartialEq)]
 pub enum UciCommand {
-    Unknown(String),
-    UciNewGame,
     Uci,
+    UciNewGame,
     IsReady,
-    Position(String, Vec<String>),
-    Go(String),
     Quit,
     Stop,
+    Position(String, Vec<String>),
+    Go(String),
     Perft(u8),
     Option(String, String),
+    Unknown(String),
 }
 
 impl From<&str> for UciCommand {
     fn from(line: &str) -> Self {
-        if line.starts_with("ucinewgame") {
-            return UciCommand::UciNewGame;
-        } else if line.starts_with("setoption") {
-            let mut words = line.split_whitespace();
-            let mut name_parts = Vec::new();
-            let mut value_parts = Vec::new();
+        let mut tokens = line.trim().split_whitespace();
 
-            // parse option name
-            while let Some(word) = words.next() {
-                if word == "value" {
-                    break;
-                } else {
-                    name_parts.push(word);
-                }
+        return if let Some(cmd_token) = tokens.next() {
+            match cmd_token {
+                "uci" => UciCommand::Uci,
+                "ucinewgame" => UciCommand::UciNewGame,
+                "isready" => UciCommand::IsReady,
+                "quit" => UciCommand::Quit,
+                "stop" => UciCommand::Stop,
+                // Unknown command
+                _ => UciCommand::Unknown(line.to_owned()),
             }
-            for word in words {
-                value_parts.push(word);
-            }
-            let name = name_parts.last().unwrap();
-            let value = value_parts.last().unwrap_or(&"");
-            return UciCommand::Option(name.parse().unwrap(), value.parse().unwrap());
-        } else if line.starts_with("uci") {
-            return UciCommand::Uci;
-        } else if line.starts_with("isready") {
-            return UciCommand::IsReady;
-        } else if line.starts_with("go") {
-            return UciCommand::Go("".to_owned());
-        } else if line.starts_with("position") {
-            let mut moves = Vec::new();
-            if line.contains("moves") {
-                if let Some(moves_) = line.split_terminator("moves ").nth(1) {
-                    for mov in moves_.split_whitespace() {
-                        moves.push(String::from(mov));
-                    }
-                }
-            }
-            return UciCommand::Position("".to_owned(), moves);
-        } else if line.starts_with("quit") {
-            return UciCommand::Quit;
-        } else if line.starts_with("perft") {
-            let depth = line
-                .split_whitespace()
-                .nth(1)
-                .and_then(|d| d.parse().ok())
-                .unwrap_or(6);
-            return UciCommand::Perft(depth);
-        } else if line == "stop" {
-            return UciCommand::Stop;
-        }
-        Self::Unknown(line.to_owned())
+        } else {
+            // Unknown (empty) command
+            UciCommand::Unknown(line.to_owned())
+        };
     }
 }
 
@@ -105,6 +71,13 @@ mod tests {
     fn should_parse_stop() {
         let actual = UciCommand::from("stop");
         let expected = UciCommand::Stop;
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn should_parse_unknown_command() {
+        let actual = UciCommand::from("asdjasjgasbdfoa");
+        let expected = UciCommand::Unknown("asdjasjgasbdfoa".to_owned());
         assert_eq!(actual, expected);
     }
 }
