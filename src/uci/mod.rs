4d66b67bd1277501135ca127c05aeedddbd04578
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-11-29 19:02:17 +0100

    Move UCI command to separate file

--- src/uci/mod.rs
+++ src/uci/mod.rs
@@ -1,1 +1,2 @@
 pub mod uci;
+mod uci_command;
--- src/uci/uci.rs
+++ src/uci/uci.rs
@@ -6,6 +6,8 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::{io, sync, thread};
 
+use super::uci_command::UciCommand;
+
 pub trait UciEngine {
     /// Create a new engine instance.
     fn new() -> Self
@@ -114,73 +116,5 @@ impl UciRunner {
                 }
             }
         }
-    }
-}
-
-pub enum UciCommand {
-    Unknown(String),
-    UciNewGame,
-    Uci,
-    IsReady,
-    Position(String, Vec<String>),
-    Go(String),
-    Quit,
-    Stop,
-    Perft(u8),
-    Option(String, String),
-}
-
-impl From<&str> for UciCommand {
-    fn from(line: &str) -> Self {
-        if line.starts_with("ucinewgame") {
-            return UciCommand::UciNewGame;
-        } else if line.starts_with("setoption") {
-            let mut words = line.split_whitespace();
-            let mut name_parts = Vec::new();
-            let mut value_parts = Vec::new();
-
-            // parse option name
-            while let Some(word) = words.next() {
-                if word == "value" {
-                    break;
-                } else {
-                    name_parts.push(word);
-                }
-            }
-            for word in words {
-                value_parts.push(word);
-            }
-            let name = name_parts.last().unwrap();
-            let value = value_parts.last().unwrap_or(&"");
-            return UciCommand::Option(name.parse().unwrap(), value.parse().unwrap());
-        } else if line.starts_with("uci") {
-            return UciCommand::Uci;
-        } else if line.starts_with("isready") {
-            return UciCommand::IsReady;
-        } else if line.starts_with("go") {
-            return UciCommand::Go("".to_owned());
-        } else if line.starts_with("position") {
-            let mut moves = Vec::new();
-            if line.contains("moves") {
-                if let Some(moves_) = line.split_terminator("moves ").nth(1) {
-                    for mov in moves_.split_whitespace() {
-                        moves.push(String::from(mov));
-                    }
-                }
-            }
-            return UciCommand::Position("".to_owned(), moves);
-        } else if line.starts_with("quit") {
-            return UciCommand::Quit;
-        } else if line.starts_with("perft") {
-            let depth = line
-                .split_whitespace()
-                .nth(1)
-                .and_then(|d| d.parse().ok())
-                .unwrap_or(6);
-            return UciCommand::Perft(depth);
-        } else if line == "stop" {
-            return UciCommand::Stop;
-        }
-        Self::Unknown(line.to_owned())
     }
 }
