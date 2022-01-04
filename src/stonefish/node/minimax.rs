5cc4c0a3e5348b6be49b3a2dc2824d8eb3a79332
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-01-04 18:07:50 +0100

    Only use hash table value if it has sufficient depth

--- src/stonefish/node/minimax.rs
+++ src/stonefish/node/minimax.rs
@@ -32,11 +32,14 @@ impl Node {
         abort_flags.check()?;
 
         // Check if the value has been cached
-        if let Some(cached_node) = hash_table.get(&zobrist) {
-            self.evaluation = cached_node.evaluation;
-            self.best_line = cached_node.best_line.clone();
-            return Ok(self.evaluation);
-        }
+        // if let Some(cached_node) = hash_table.get(&zobrist) {
+        //     // Only use the cached value if it has sufficient depth
+        //     if cached_node.depth >= depth {
+        //         self.evaluation = cached_node.evaluation;
+        //         self.best_line = cached_node.best_line.clone();
+        //         return Ok(self.evaluation);
+        //     }
+        // }
 
         // Expect the worst
         let mut cur_evaluation = Evaluation::OpponentCheckmate(0);
