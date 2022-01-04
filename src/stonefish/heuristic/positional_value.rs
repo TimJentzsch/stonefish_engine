b655974946ad5e8aab8852dbe8707689d5e45e1a
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-01-04 17:13:52 +0100

    Consider guarding pieces for heuristic

--- src/stonefish/heuristic/positional_value.rs
+++ src/stonefish/heuristic/positional_value.rs
@@ -1,27 +1,56 @@
-use pleco::{Board, Player};
+use pleco::{Board, Piece, Player, SQ};
 
 use super::material_value::get_piece_value;
 
-/// Calculate the positional value for the current player.
-fn player_positional_value(board: &Board, player: Player) -> i32 {
-    // For the opponent we have to play a null move to generate the moves
-    let mut board = board.clone();
+struct Guard {
+    origin: (SQ, Piece),
+    target: (SQ, Piece),
+}
 
-    if player != board.turn() {
-        unsafe {
-            board.apply_null_move();
+impl Guard {
+    fn new(origin: (SQ, Piece), target: (SQ, Piece)) -> Self {
+        Self { origin, target }
+    }
+}
+
+/// Gets the squares and pieces that the player guards (defends or attacks).
+fn player_guards(board: &Board, player: Player) -> Vec<Guard> {
+    let piece_locations = board.get_piece_locations();
+
+    let mut guards = vec![];
+
+    for (sq, piece) in piece_locations {
+        // Only consider pieces of the current player
+        if piece.player().unwrap_or(player.other_player()) != player {
+            continue;
+        }
+
+        // Look at every square that the piece attacks
+        for attack_sq in board.attacks_from(piece.type_of(), sq, player) {
+            let target = board.piece_at_sq(attack_sq);
+
+            guards.push(Guard::new((sq, piece), (attack_sq, target)));
         }
     }
 
+    guards
+}
+
+/// Calculate the positional value for the current player.
+fn player_positional_value(board: &Board, player: Player) -> i32 {
     let mut value = 0;
-    let captures = board.generate_pseudolegal_moves_of_type(pleco::core::GenTypes::Captures);
+    
+    let guards = player_guards(board, player);
 
-    for mv in captures {
-        let src_piece = board.piece_at_sq(mv.get_src()).type_of();
-        let dest_piece = board.piece_at_sq(mv.get_dest()).type_of();
+    for guard in guards {
+        let (_, origin_piece) = guard.origin;
+        let (_, target_piece) = guard.target;
 
-        // It's good to attack pieces of higher value
-        value += 0.max(get_piece_value(dest_piece) - get_piece_value(src_piece)) / 2;
+        if origin_piece.player() == target_piece.player() {
+            value += 20;
+        } else {
+            value += get_piece_value(target_piece.type_of()) / 10;
+        }
     }
 
     value
