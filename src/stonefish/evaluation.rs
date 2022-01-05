185120025c7e48f433e85faeaa910544d1871d11
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-01-05 22:46:06 +0100

    Fix evaluation module inception

--- src/stonefish/evaluation.rs
+++ src/stonefish/evaluation.rs
@@ -1,6 +1,18 @@
+/// The evaluation of a given position.
+///
+/// Smaller values mean an advantage for the opponent, bigger values an advantage for the current player.
+#[derive(Debug, Clone, Copy)]
+pub enum Evaluation {
+    /// A material evaluation in centipawns.
+    ///
+    /// Negative numbers are an advantage for the opponent, positive numbers an advantage for the current player.
+    Material(i32),
+    /// The current player can give checkmate in the given number of plies.
+    PlayerCheckmate(usize),
+    /// The opponent can give checkmate in the given number of plies.
+    OpponentCheckmate(usize),
+}
 use std::cmp::Ordering;
-
-use super::Evaluation;
 
 impl Evaluation {
     pub fn for_opponent(&self) -> Self {
