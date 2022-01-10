6dbdc4cbafb32f23d7bf9379f818c04879007a72
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-01-10 17:56:36 +0100

    Further improve positional evaluation

--- src/stonefish/heuristic/positional_value.rs
+++ src/stonefish/heuristic/positional_value.rs
@@ -39,15 +39,44 @@ fn player_guards(board: &Board, player: Player) -> Vec<Guard> {
     guards
 }
 
-/// Generate a bitboard for the board borders.
+/// The borders of the board.
 #[inline]
 fn get_border_bb() -> BitBoard {
     BitBoard::FILE_A | BitBoard::FILE_H | BitBoard::RANK_1 | BitBoard::RANK_8
 }
 
-/// Counts the number of pieces at the borders of the board.
-fn count_border_pieces(piece_bb: BitBoard) -> u8 {
-    (get_border_bb() & piece_bb).count_bits()
+/// The corners of the board.
+#[inline]
+fn get_corner_bb() -> BitBoard {
+    SQ::A1.to_bb() | SQ::A8.to_bb() | SQ::H1.to_bb() | SQ::H8.to_bb()
+}
+
+/// The center of the board with radius one.
+#[inline]
+fn get_center_sq_one_bb() -> BitBoard {
+    SQ::D4.to_bb() | SQ::E4.to_bb() | SQ::D5.to_bb() | SQ::E5.to_bb()
+}
+
+/// Get the center ring of the board with radius two.
+#[inline]
+fn get_center_ring_two_bb() -> BitBoard {
+    (BitBoard::FILE_C | BitBoard::FILE_D | BitBoard::FILE_E | BitBoard::FILE_F)
+        & (BitBoard::RANK_1
+            | BitBoard::RANK_2
+            | get_center_sq_one_bb()
+            | BitBoard::RANK_7
+            | BitBoard::RANK_8)
+}
+
+/// Get the center the board with radius two.
+#[inline]
+fn get_center_sq_two_bb() -> BitBoard {
+    get_center_sq_one_bb() | get_center_ring_two_bb()
+}
+
+/// Score how many pieces have the corresponding positions.
+fn score_position(piece_bb: BitBoard, position_bb: BitBoard, score: i32) -> i32 {
+    (piece_bb & position_bb).count_bits() as i32 * score
 }
 
 /// Determine if the player is in the endgame.
@@ -69,17 +98,18 @@ fn is_endgame(board: &Board) -> bool {
 
 /// Evaluate the position of the king.
 fn player_king_position(board: &Board, player: Player) -> i32 {
-    let king_bb = board.piece_bb(player, PieceType::K);
+    let piece_bb = board.piece_bb(player, PieceType::K);
 
     if is_endgame(board) {
         let mut value = 0;
 
         // Stay away from the borders
-        value += count_border_pieces(king_bb) as i32 * -40;
+        value += score_position(piece_bb, get_border_bb(), -30);
+        value += score_position(piece_bb, get_corner_bb(), -20);
 
         // Go to the center
-        let center_bb = SQ::D4.to_bb() | SQ::E4.to_bb() | SQ::D5.to_bb() | SQ::E5.to_bb();
-        value += (king_bb & center_bb).count_bits() as i32 * 30;
+        value += score_position(piece_bb, get_center_sq_one_bb(), 40);
+        value += score_position(piece_bb, get_center_ring_two_bb(), 20);
 
         value
     } else {
@@ -90,30 +120,22 @@ fn player_king_position(board: &Board, player: Player) -> i32 {
             Player::White => SQ::G1.to_bb() | SQ::C1.to_bb(),
             Player::Black => SQ::G8.to_bb() | SQ::C8.to_bb(),
         };
-        value += (king_bb & castle_bb).count_bits() as i32 * 50;
+        value += (piece_bb & castle_bb).count_bits() as i32 * 50;
 
         // Don't stand around in the center
         let center_bb = match player {
             Player::White => SQ::E1.to_bb() | SQ::D1.to_bb() | SQ::E2.to_bb() | SQ::D2.to_bb(),
             Player::Black => SQ::E8.to_bb() | SQ::D8.to_bb() | SQ::E7.to_bb() | SQ::D7.to_bb(),
         };
-        value += (king_bb & center_bb).count_bits() as i32 * -20;
+        value += (piece_bb & center_bb).count_bits() as i32 * -20;
 
         value
     }
 }
 
-/// Evaluate the position of the knights.
-fn player_knight_position(board: &Board, player: Player) -> i32 {
-    let knight_bb = board.piece_bb(player, PieceType::N);
-
-    // Avoid having knights on the borders
-    count_border_pieces(knight_bb) as i32 * -30
-}
-
 /// Evaluate the position of the pawns.
 fn player_pawn_position(board: &Board, player: Player) -> i32 {
-    let pawn_bb = board.piece_bb(player, PieceType::P);
+    let piece_bb = board.piece_bb(player, PieceType::P);
     let mut value = 0;
 
     // Being close to promotion is good
@@ -121,13 +143,13 @@ fn player_pawn_position(board: &Board, player: Player) -> i32 {
         Player::White => BitBoard::RANK_7,
         Player::Black => BitBoard::RANK_2,
     };
-    value += (rank_seven_bb & pawn_bb).count_bits() as i32 * 50;
+    value += (rank_seven_bb & piece_bb).count_bits() as i32 * 50;
 
     let rank_six_bb = match player {
         Player::White => BitBoard::RANK_6,
         Player::Black => BitBoard::RANK_3,
     };
-    value += (rank_six_bb & pawn_bb).count_bits() as i32 * 30;
+    value += (rank_six_bb & piece_bb).count_bits() as i32 * 30;
 
     let center_files_bb = BitBoard::FILE_D | BitBoard::FILE_E;
 
@@ -136,35 +158,57 @@ fn player_pawn_position(board: &Board, player: Player) -> i32 {
         Player::White => BitBoard::RANK_5 & center_files_bb,
         Player::Black => BitBoard::RANK_4 & center_files_bb,
     };
-    value += (rank_five_center_bb & pawn_bb).count_bits() as i32 * 25;
+    value += (rank_five_center_bb & piece_bb).count_bits() as i32 * 25;
 
     let rank_four_center_bb = match player {
         Player::White => BitBoard::RANK_4 & center_files_bb,
         Player::Black => BitBoard::RANK_5 & center_files_bb,
     };
-    value += (rank_four_center_bb & pawn_bb).count_bits() as i32 * 20;
+    value += (rank_four_center_bb & piece_bb).count_bits() as i32 * 20;
 
     // Not developing the center pawns is bad
     let rank_two_center_bb = match player {
         Player::White => BitBoard::RANK_2 & center_files_bb,
         Player::Black => BitBoard::RANK_7 & center_files_bb,
     };
-    value += (rank_two_center_bb & pawn_bb).count_bits() as i32 * -20;
+    value += (rank_two_center_bb & piece_bb).count_bits() as i32 * -20;
+
+    value
+}
+
+/// Evaluate the position of the knights.
+fn player_knight_position(board: &Board, player: Player) -> i32 {
+    let piece_bb = board.piece_bb(player, PieceType::N);
+    let mut value = 0;
+
+    // Avoid having knights on the borders
+    value += score_position(piece_bb, get_border_bb(), -30);
+    value += score_position(piece_bb, get_corner_bb(), -20);
+    // Move knights to the center
+    value += score_position(piece_bb, get_center_sq_one_bb(), 20);
+    value += score_position(piece_bb, get_center_ring_two_bb(), 10);
 
     value
 }
 
 /// Evaluate the position of the bishops.
 fn player_bishop_position(board: &Board, player: Player) -> i32 {
-    let bishop_bb = board.piece_bb(player, PieceType::B);
+    let piece_bb = board.piece_bb(player, PieceType::B);
+    let mut value = 0;
 
     // Avoid having bishops on the borders
-    count_border_pieces(bishop_bb) as i32 * -25
+    value += score_position(piece_bb, get_border_bb(), -15);
+    value += score_position(piece_bb, get_corner_bb(), -10);
+    // Move bishops to the center
+    value += score_position(piece_bb, get_center_sq_one_bb(), 15);
+    value += score_position(piece_bb, get_center_ring_two_bb(), 10);
+
+    value
 }
 
 /// Evaluate the position of the rooks.
 fn player_rook_position(board: &Board, player: Player) -> i32 {
-    let rook_bb = board.piece_bb(player, PieceType::R);
+    let piece_bb = board.piece_bb(player, PieceType::R);
     let mut value = 0;
 
     // Being on the 7th rank is good
@@ -172,28 +216,34 @@ fn player_rook_position(board: &Board, player: Player) -> i32 {
         Player::White => BitBoard::RANK_7,
         Player::Black => BitBoard::RANK_2,
     };
-    value += (rank_seven_bb & rook_bb).count_bits() as i32 * 15;
+    value += score_position(piece_bb, rank_seven_bb, 15);
 
     // Being in the center is good
     let center_bb = match player {
         Player::White => SQ::D1.to_bb() | SQ::E1.to_bb(),
         Player::Black => SQ::D8.to_bb() | SQ::E8.to_bb(),
     };
-    value += (center_bb & rook_bb).count_bits() as i32 * 10;
+    value += score_position(piece_bb, center_bb, 10);
 
     // Avoid the left and right borders
-    let border_bb = (BitBoard::FILE_A | BitBoard::FILE_H) & (BitBoard::RANK_1 | BitBoard::RANK_8);
-    value += (border_bb & rook_bb).count_bits() as i32 * -5;
+    let border_bb = (BitBoard::FILE_A | BitBoard::FILE_H) ^ get_border_bb();
+    value += score_position(piece_bb, border_bb, -5);
 
     value
 }
 
 /// Evaluate the position of the queens.
 fn player_queen_position(board: &Board, player: Player) -> i32 {
-    let queen_bb = board.piece_bb(player, PieceType::Q);
+    let piece_bb = board.piece_bb(player, PieceType::Q);
+    let mut value = 0;
 
     // Avoid having queens on the borders
-    count_border_pieces(queen_bb) as i32 * -10
+    value += score_position(piece_bb, get_border_bb(), -10);
+    value += score_position(piece_bb, get_corner_bb(), -10);
+    // Move the queen to the center
+    value += score_position(piece_bb, get_center_sq_two_bb(), 5);
+
+    value
 }
 
 /// The total piece position for the player.
