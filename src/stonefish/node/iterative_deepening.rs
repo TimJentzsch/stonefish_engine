93525fbf9eeb3588692bb1147f0df5efb15cfd70
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-01-18 17:32:37 +0100

    Fix bug with hash table values used when creating new nodes The hash table values would be taken as evaluation of the node. However, this field takes the initial evaluation, but the hash table provides final evaluations. This resulted in incorrect evaluations as the nodes were updated with every move.

--- src/stonefish/node/iterative_deepening.rs
+++ src/stonefish/node/iterative_deepening.rs
@@ -205,7 +205,7 @@ mod tests {
             ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 3),
             ("8/r1N1k1pp/b4p2/3Qp3/P6q/4P3/2PP1PPP/R3K2R w KQ - 1 25", 3),
             // TODO: Fix mate here
-            // ("4r3/1b2rpk1/p6R/1p4p1/2pN4/P1P2P2/1P3KP1/R7 w - - 2 29", 5),
+            ("4r3/1b2rpk1/p6R/1p4p1/2pN4/P1P2P2/1P3KP1/R7 w - - 2 29", 5),
         ];
 
         for (fen, depth) in paramerters {
